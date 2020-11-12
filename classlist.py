bd27a5d6aa64741c62116c4de40ce8beb34948dd
Anh-VAnh <71421280+Anh-VAnh@users.noreply.github.com>
2020-11-12 11:26:32 +0700

    Add files via upload

---
+++ classlist.py
@@ -0,0 +1,105 @@
+import os
+# creating .tex file from with python
+import sys
+
+def create_student_data_texfile(student_data):
+    """Create student data texfile (student_data.tex)"""
+    print(student_data)
+    filename = "student_data.tex"
+    outfile = open(filename,'w')
+    count = 0
+    for student in student_data:
+        print(student)
+        student_id, student_name, student_date_of_birth, student_training_program = student
+        print(student_id, student_name, student_date_of_birth, student_training_program)
+        count += 1
+        outfile.write(f"""{count} & {student_id} & {student_name} & {student_date_of_birth} & {student_training_program} & & & & \\\\
+\\hline
+""")
+   
+    outfile.close() 
+#    1 & 12345678 & Nguyễn Y Vân & 21/10/1997 & 28CLCMTTT & 1 & $ \gamma\upsilon $ & 2 & MẤT TÍCH\\
+def import_student_data(filename):
+    """This function will import student data from a csv file(classlist.dat)"""
+    print(f"Importing student data from file '{filename}'")
+    datafile = open(filename,'r')
+    student_data = []
+    lines = datafile.readlines()
+    for line in lines:
+#        print(line)
+        student_id, student_name, student_date_of_birth, student_training_program = line.split(',')
+        student_id = student_id.strip()
+        student_name = student_name.strip()
+        student_date_of_birth = student_date_of_birth.strip()
+        student_training_program = student_training_program.strip()
+#        print(f"Mã sinh viên '{student_id}', Họ và tên '{student_name}', Ngày sinh '{student_date_of_birth}', Chương trình đào tạo '{student_training_program}'")
+        student_data.append([student_id, student_name, student_date_of_birth, student_training_program])
+    datafile.close()
+    return student_data
+
+# chuong trinh chinh bat dau tu day
+def main():
+    """This is the main program"""
+    script, semester, academic_year = sys.argv
+    textfilename = "newclasslist.tex"
+    fout = open(textfilename,"w")
+    content = r"""\documentclass[12pt,a4paper]{book}
+\usepackage[utf8]{vietnam}
+\usepackage[left=0.2cm, right=1cm, top=1cm, bottom=2.00cm]{geometry} %%Set border
+\newcommand\tab[1][1cm]{\hspace*{#1}} %% Creat tab
+\setlength{\parindent}{0pt}
+\usepackage{scrextend}
+\changefontsizes{8pt}
+\usepackage{setspace} %% Paragraph 2
+\usepackage{xhfill} %% Dong ngang
+\usepackage{array} 
+\usepackage{multirow} %% Merge
+\usepackage{booktabs} %%Change width line in table
+\usepackage{blindtext}
+\begin{document}
+\centerline{\textbf{%s %s}}
+\pagenumbering{gobble} %% No number this page
+\newcommand{\xfill}[2][1ex]{{
+		\dimen0=#2\advance\dimen0 by #1
+		\leaders\hrule height \dimen0 depth -#1\hfill
+}}
+\textbf{Học phần: Nhập môn Internet kết nối vạn vật}\tab[3cm]Lớp HP: \textbf{PHY1070 3}\tab[2cm]Số tín chỉ: 2\\
+Thứ: 4\tab[9.1cm]Tiết: 9 - 11\tab[3.45cm]Giảng đường: 201T5\\
+\begin{tabular}{ m{0.7cm} | m{2cm}| m{4cm} | m{2cm} | m{2.5cm} | m{1.2cm} | m{1.5cm} | m{1cm} | m{1.6cm} | }
+		\toprule[0.4mm]
+		\begin{center}\textbf{STT }\end{center}&\begin{center}\textbf{Mã SV}\end{center} &\begin{center}\textbf{Họ và tên}\end{center} &\begin{center} \textbf{Ngày sinh}\end{center} &\begin{center} \textbf{Lớp}\end{center} &\vspace{-0.2cm}\begin{center} \textbf{Thường\vspace{-0.2cm}\\xuyên\vspace{-0.2cm}\\(...\%%)\vspace{-0.6cm} }\end{center} & \begin{center} \textbf{Ký nộp }\end{center}&\vspace{-0.2cm}\begin{center} \textbf{Giữa\vspace{-0.2cm}\\kỳ\vspace{-0.2cm}\\(...\%%)\vspace{-0.6cm} }\end{center} &\begin{center} \textbf{Ghi chú}\vspace{-0cm}\end{center}\\
+		\hline
+\input{student_data.tex}
+		\hline
+	\bottomrule[0.4mm]
+\end{tabular}
+\end{document}
+""" %(semester, academic_year)
+    fout.write(content)
+    fout.close()
+# creating .pdf file from .tex file with xelatex
+    cmd = f"pdflatex {textfilename}"
+    failure = os.system(cmd)
+    if failure:
+        print(f'Creating {textfilename} failure!!!')
+    else:
+        print(f'Creating {textfilename} successfully!!!')
+
+    pdffilename = textfilename[:-3]+'pdf'
+    print(pdffilename)
+# Viewing classlist.pdf
+    cmd = f"evince {pdffilename} &"
+    failure = os.system(cmd)
+    if failure:
+        print(f'Viewing {pdffilename} failed!!!')
+    else:
+        print(f'Viewing {pdffilename} successfully!!!')
+    print(sys.argv)
+    print("Chuong trinh la %s abcd %s cdf %s"%(script, semester, academic_year))
+
+    filename = 'classlist.dat'
+    student_data = import_student_data(filename)
+    create_student_data_texfile(student_data)
+
+if __name__ == "__main__":
+    main()    
